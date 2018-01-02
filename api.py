93f810822c45b7ae423d0016a813344315c25189
nishnash54 <nishnash54@gmail.com>
2018-01-02 22:53:11 +0530

    Ready for subConn

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from hash_gen import hash
-from database import update
+from database import update, validate
 from json import dumps
 
 app = Flask(__name__)
@@ -19,6 +19,10 @@ def api_transaction():
     update(data, data_hash, last_transaction)
     last_transaction = data_hash
     return last_transaction
+
+@app.route("/validate")
+def api_validate():
+    return validate("3b7011da3be422a3afc91d31cb824281b051c3e7fa924a871722f04f6d134965")
 
 if __name__ == '__main__':
     app.run(debug = True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from config import var
-from json import loads
+from json import dumps, loads
+from hash_gen import hash
 
 client = MongoClient()
 client = MongoClient(var.link, 27017)
@@ -10,3 +11,21 @@ def update(data, hash, tid):
     data = loads(data)
     db.transactions.insert({"from_id": data['from_id'], "to_id": data['to_id'],\
         "last_item_transaction": data['transaction_id'], "transaction_id": hash, "previous_transaction": tid})
+
+def validate(tid):
+    flag = 1
+    lst = []
+    while(tid!=""):
+        cur = db.transactions.find({"transaction_id": tid})[0]
+        last = cur['previous_transaction']
+        #print "tid : ", tid
+        #print "last : ", last
+        #print "cur : ", cur['transaction_id']
+        data = dumps({"from_id": cur['from_id'], "to_id": cur['to_id'],\
+            "transaction_id": cur['last_item_transaction'], "last_transaction": cur['previous_transaction']})
+        val = hash(data)
+        if val!=tid:
+            flag = 0
+            break
+        tid = last
+    return str(flag)
